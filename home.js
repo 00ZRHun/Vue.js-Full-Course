6f2f787df2f9074af6e91b72f76e80e232c27b12
00ZRHun <00ZRHun@gmail.com>
2020-04-15 18:20:15 +0800

    Class & Style Binding

--- home.js
+++ home.js
@@ -3,13 +3,14 @@
 // Vue instance
 var app = new Vue({
     el: '#app',
+    // property for data
     data: {
         product: 'Socks',
         description: 'A pair of warm fuzzy socks',
         image: './assets/vmSocks-green.jpg',
         altText: 'A pair of socks',
         link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-        inStock: true,
+        inStock: false,
         inventory: 100,
         onSale: false,
         details: ["80% cotton", "20% polyster", "Gender-nuetral"],
@@ -27,8 +28,17 @@ var app = new Vue({
         ],
         sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
         cart: 0,
+        styleObject1: {
+                color: "red",
+                fontSize: '20px',
+        },
+        styleObject2: {
+                backgroundColor: 'black'
+        },
     },
+    // methods property
     methods: {
+        // anonymous function
         addToCart: function () {
             this.cart += 1;
         },
@@ -37,7 +47,8 @@ var app = new Vue({
         updateProduct(variantImage) {
             this.image = variantImage;
         },
-        minusToCart() {
+        removeFromCart() {
+            if (this.cart > 0)
             this.cart -= 1;
         },
     },
