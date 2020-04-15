481f9656510dce8e92e2f9aceeda598fcbfa36ac
00ZRHun <00ZRHun@gmail.com>
2020-04-15 13:00:10 +0800

    list rendering

--- home.js
+++ home.js
@@ -4,13 +4,26 @@
 var app = new Vue({
     el: '#app',
     data: {
-        product: 'Boots',
+        product: 'Socks',
         description: 'A pair of warm fuzzy socks',
         image: './assets/vmSocks-green.jpg',
         altText: 'A pair of socks',
         link: 'https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Daps&field-keywords=socks',
-        inStock: false,
+        inStock: true,
         inventory: 100,
         onSale: false,
+        details: ["80% cotton", "20% polyster", "Gender-nuetral"],
+        variants: [
+            {
+                variantID: 2234,
+                variantColor: "green"
+            },
+            {
+                variantID: 2235,
+                variantColor: "blue"
+            },
+        ],
+        sizes: ['S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
     },
+    
 })
